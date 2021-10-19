0719d725969c2982966ca9057a7603ddd09260fa
Anderson <andersonribeiro.si@hotmail.com>
2021-10-19 14:33:58 -0300

    Passando argumentos em rotas nomeadas

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -2,14 +2,11 @@ import 'package:coder_shop/models/product.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailPage extends StatelessWidget {
-  final Product product;
-  const ProductDetailPage({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
+  const ProductDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Product product = ModalRoute.of(context)?.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         actions: [],
