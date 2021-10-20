fd8124e460abb4a479b09621b07826b686ad5fe5
Anderson <andersonribeiro.si@hotmail.com>
2021-10-20 20:12:43 -0300

    Refatorando o GridView de produtos

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:coder_shop/components/product_item.dart';
 import 'package:coder_shop/data/dummy_data.dart';
 import 'package:coder_shop/models/product.dart';
@@ -11,9 +10,6 @@ class ProductsOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -25,19 +21,28 @@ class ProductsOverviewPage extends StatelessWidget {
           ),
         )),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: dummyProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(
-          product: loadedProducts[i],
-        ),
-        // ignore: prefer_const_constructors
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
+      body: ProductGrid(),
+    );
+  }
+}
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    List<Product> loadedProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: dummyProducts.length,
+      itemBuilder: (ctx, i) => ProductItem(
+        product: loadedProducts[i],
+      ),
+      // ignore: prefer_const_constructors
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
       ),
     );
   }
